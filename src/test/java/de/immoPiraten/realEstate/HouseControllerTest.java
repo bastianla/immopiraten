541ae3408c06423a028a7e7f89757ee58ce6522f
bastianla <sebastian.lataster@smail.th-koeln.de>
2017-04-17 19:16:45 +0200

    Erster Versuch JUnitTest.

---
+++ src/test/java/de/immoPiraten/realEstate/HouseControllerTest.java
@@ -0,0 +1,34 @@
+package de.immoPiraten.realEstate;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class HouseControllerTest {
+
+	@Autowired
+	private MockMvc mvc;
+	
+	@Test
+	public void getHouse() throws Exception {
+		mvc.perform(
+				MockMvcRequestBuilders.get("/")
+				.accept(MediaType.APPLICATION_JSON)
+				// .andExpect(status().isOk())
+				);
+	}
+}
