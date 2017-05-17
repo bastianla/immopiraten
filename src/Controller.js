317945fe9ae4a313ac9dbaafca193c20cc7d70e2
Andrea Mergenbaum <andrea.mergenbaum@gmx.de>
2017-05-17 18:44:06 +0200

    Anzeige eines Exposes über Json

--- src/Controller.js
+++ src/Controller.js
@@ -7,8 +7,15 @@ var app = angular.module('ImmoPiratenApp', []);
     });
 });*/
 app.controller('customersCtrl', function($scope, $http) {
-	    $http.get("https://www.w3schools.com/angular/customers.php").then(function(response) {
-	    	$scope.myData = response.data.records;
-	    
+    $http.get("https://www.w3schools.com/angular/customers.php").then(function(response) {
+    	$scope.myData = response.data.records;	    
 	});
+});
+
+app.controller('exposeCtrl', function($scope, $http) {
+    $http.get("http://localhost:8080/exposeJson?id=92756718").then(function(response) {
+    	$scope.exposedata = response.data['expose:expose'];  
+    	$scope.exposedata.publishDate = response.data['expose:expose'][publishDate];    
+    	
+});
 });
