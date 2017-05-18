c1de4c9bd425769783c2266bcbfd598f9ba727fa
PCiecior <peter.ciecior@gmx.de>
2017-05-18 19:22:50 +0200

    Add files via upload

---
+++ src/main/java/de/RealEstateService/RealEstateServiceApplication.java
@@ -0,0 +1,12 @@
+package de.RealEstateService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealEstateServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealEstateServiceApplication.class, args);
+	}
+}
