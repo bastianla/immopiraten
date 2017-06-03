b695925a591bf08955ec0bd972202a339fa220fc
Andrea Mergenbaum <andrea.mergenbaum@gmx.de>
2017-06-03 12:22:20 +0200

    erweiterte Suche - Auswahl Suchservices (nur GUI) - Kleine Styling Anpassungen

--- src/Controller.js
+++ src/Controller.js
@@ -24,14 +24,36 @@ app.directive('onlyDigits', function () {
 
 app.controller('searchCtrl', function($scope, $http, $location) {
 	
-	/*$scope.doSearch = function() {
-		$scope.debug="Debuger:";
-	    //$http.get("http://localhost:8080/houses?postcode="+$scope.postcode+"&city=Essen&pricefrom=0&pricetill=1000&livingareafrom=0&livingareatill=1000&landareafrom=0&landareatill=10000&roomfrom=1&roomtill=5&constructionyearfrom=0&constructionyeartill=1000&balcony=1&terrace=0&garden=0&garage=0&commission=0").then(function(response) {
-	    	$http.get("http://localhost:8080/search?realestatetype=1&purchasetype=1&input=aachen&radius=20").then(function(response) {
-			$scope.exposedata = response.data;  
-	    	$scope.exposedata.title = response.data[0].title; 
-	    });
-    }*/
+	$scope.followBtnImgUrlImmoScout = '/images/erweiterte_suche/u210.png'
+
+    $scope.toggleImageImmoScout = function () {
+        if ($scope.followBtnImgUrlImmoScout === '/images/erweiterte_suche/u210.png') {
+        	if($scope.immoNet!=='0'){
+	            $scope.followBtnImgUrlImmoScout = '/images/erweiterte_suche/u211.png';
+	            $scope.immoScout='0';
+        	}
+        } else {
+            $scope.followBtnImgUrlImmoScout = '/images/erweiterte_suche/u210.png';
+            $scope.immoScout='1';
+        }
+    }
+	
+	$scope.followBtnImgUrlImmoNet = '/images/erweiterte_suche/u210.png'
+
+	    $scope.toggleImageImmoNet = function () {
+	        if ($scope.followBtnImgUrlImmoNet === '/images/erweiterte_suche/u210.png') {
+	        	if($scope.immoScout!=='0'){
+		        	$scope.followBtnImgUrlImmoNet = '/images/erweiterte_suche/u211.png';
+		            $scope.immoNet='0';
+	        	}
+	        } else {
+	            $scope.followBtnImgUrlImmoNet = '/images/erweiterte_suche/u210.png';
+	            $scope.immoNet='1';
+	        }
+	    }
+	
+	
+	
     $scope.getDetail = function() {	
 				
 		$scope.realestatetype = $location.search()['realestatetype'];
@@ -54,6 +76,7 @@ app.controller('searchCtrl', function($scope, $http, $location) {
     	
 		var searchparams="realestatetype="+$scope.realestatetype+"&purchasetype="+$scope.purchasetype+"&input="+$scope.city;
 		
+		//hier fehlt noch die Einbindung der Parameter immoscout und immonet um die ausgewählten Services anzufragen
 		
 		if (typeof $scope.radius === 'undefined') {
 			$scope.radius = '100';
