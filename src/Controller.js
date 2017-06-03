df8f08eacc012cc705f697952ce3c9d7d7f4dcf9
Andrea Mergenbaum <andrea.mergenbaum@gmx.de>
2017-06-03 10:36:09 +0200

    Startseite + erweiterete Suche Error Handling und Ausführung

--- src/Controller.js
+++ src/Controller.js
@@ -1,38 +1,26 @@
-var app = angular.module('ImmoPiratenApp', ["ngRoute"]);
+var app = angular.module('ImmoPiratenApp', []);
 
-app.config(function($routeProvider) {
-    $routeProvider
-    .when("/", {
-    	templateUrl : "startseite.html",
-        controller : "searchCtrl",
-    })
-    .when("/startseite", {
-        templateUrl : "startseite.html",
-        controller : "searchCtrl",
-    })
-    .when("/erweitert", {
-        templateUrl : "erweiterte_suche.html",
-        controller : "searchCtrl",
-    });
-});
-
-
-
-app.controller('searchCtrl', function($rootScope, $scope, $http, $location) {
-    /*$http.get("http://localhost:8080/exposeJson?id=92756718").then(function(response) {
-    	$scope.exposedata = response.data['expose:expose'];  
-    	$scope.exposedata.publishDate = response.data['expose:expose'][publishDate];   
-    });	*/
-
-	$scope.doeasySearch = function() {
-
-
-	    $location.path("/erweitert/");
-        $scope.doSearch();
-    }
+app.controller('searchCtrl', function($scope, $http, $location) {
 	
-	$scope.doSearch = function() {
+	/*$scope.doSearch = function() {
+		$scope.debug="Debuger:";
+	    //$http.get("http://localhost:8080/houses?postcode="+$scope.postcode+"&city=Essen&pricefrom=0&pricetill=1000&livingareafrom=0&livingareatill=1000&landareafrom=0&landareatill=10000&roomfrom=1&roomtill=5&constructionyearfrom=0&constructionyeartill=1000&balcony=1&terrace=0&garden=0&garage=0&commission=0").then(function(response) {
+	    	$http.get("http://localhost:8080/search?realestatetype=1&purchasetype=1&input=aachen&radius=20").then(function(response) {
+			$scope.exposedata = response.data;  
+	    	$scope.exposedata.title = response.data[0].title; 
+	    });
+    }*/
+    $scope.getDetail = function() {	
 		
+		$scope.debug=$scope.city;
+		$scope.city= $location.search()['city'];
+		$scope.realestatetype = $location.search()['realestatetype'];
+		$scope.purchasetype =$location.search()['purchasetype'];
+		$scope.doSearch();
+		
+	}    	
+    
+    $scope.doSearch = function() {	
 		if (typeof $scope.radius === 'undefined') {
 				$scope.radius = '100';
 		    } 
@@ -44,17 +32,8 @@ app.controller('searchCtrl', function($rootScope, $scope, $http, $location) {
 	    	$scope.exposedata.title = response.data[0].title; 
 	    });	    
     }
-	
-	$scope.doSearchHouses = function() {
-	    $http.get("http://localhost:8080/houses?postcode="+$scope.city+"&city=Essen&pricefrom=0&pricetill=1000&livingareafrom=0&livingareatill=1000&landareafrom=0&landareatill=10000&roomfrom=1&roomtill=5&constructionyearfrom=0&constructionyeartill=1000&balcony=1&terrace=0&garden=0&garage=0&commission=0").then(function(response) {
-	    	$scope.exposedata = response.data;  
-	    	$scope.exposedata.title = response.data[0].title; 
-	    });
-    
-	
-	}
-	
-        	
 });
     
 
+
+	
